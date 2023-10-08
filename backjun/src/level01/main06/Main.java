006ede6b7f3d4928e46fc21d2604a03c7f13addc
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2023-10-08 20:47:21 +0900

    commit

--- backjun/src/level01/main06/Main.java
+++ backjun/src/level01/main06/Main.java
@@ -1,7 +1,23 @@
 package level01.main06;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		int a = scan.nextInt();
+		
+		if(a >= 90) {
+			System.out.println("A");
+		} else if((90 > a)||(a >= 80)) {
+			System.out.println("B");
+		} else if((80 > a)||(a >= 70)) {
+			System.out.println("C");
+		} else if((70 > a)||(a >= 60)) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
 	}
 }
