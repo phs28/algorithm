61ba53068ca5ceb6a487f61854a68e0e991d5dd6
phs28 <quaj628@gmail.com>
2024-02-13 18:47:05 +0900

    comit

---
+++ backjun/src/level01/main07/Main01.java
@@ -0,0 +1,18 @@
+package level01.main07;
+
+import java.util.Scanner;
+
+public class Main01 {
+	public static void main(String[] args) {
+		//2753번
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+		if((a%4==0)&&(a%100!=0||a%4==0)) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+}
