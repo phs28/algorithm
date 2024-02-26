cad324c6329824b05aaa482391c6e6df2cde7aa0
phs28 <quaj628@gmail.com>
2024-02-27 01:06:54 +0900

    commit

--- backjun/src/backjun/BOJ_2750.java
+++ backjun/src/backjun/BOJ_2750.java
@@ -1,8 +1,28 @@
 package backjun;
 
+import java.util.Scanner;
+
 public class BOJ_2750 {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+		int temp;
 		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
 		
+		for(int i=0; i<arr.length-1; i++) {
+			for(int j=i+1; j < arr.length; j++) {
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		
+		for(int val :arr) {
+			System.out.println(val);
+		}
 	}
 }
