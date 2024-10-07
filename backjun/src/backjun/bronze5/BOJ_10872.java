93a11f0ae1f141248bfd0a059aa68fb1cf67347f
phs28 <104198099+phser29@users.noreply.github.com>
2024-10-07 21:07:33 +0900

    commit

---
+++ backjun/src/backjun/bronze5/BOJ_10872.java
@@ -0,0 +1,7 @@
+package backjun.bronze5;
+
+public class BOJ_10872 {
+  public static void main(String[] args) {
+    
+  }
+}
--- backjun/src/backjun/bronze5/BOJ_18108.java
+++ backjun/src/backjun/bronze5/BOJ_18108.java
@@ -1,7 +1,17 @@
 package backjun.bronze5;
 
+import java.util.Scanner;
+
 public class BOJ_18108 {
   public static void main(String[] args) {
-    
+    Scanner scan = new Scanner(System.in);
+
+    int y = scan.nextInt();
+
+    int year = 543;
+
+    System.out.println(y - year);
+
+    scan.close();
   }  
 }
