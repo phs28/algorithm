9ce40c0ec760cb1bfa287b3e48be37ffba1a0499
phs28 <104198099+phser29@users.noreply.github.com>
2024-10-08 20:09:01 +0900

    commit

---
+++ backjun/src/book/chap04/exam02/QueenB.java
@@ -0,0 +1,14 @@
+package book.chap04.exam02;
+
+public class QueenB {
+  static int[] pos = new int[8];
+
+  static void print() {
+    
+  }
+
+  public static void main(String[] args) {
+    
+  }
+
+}
