e4dff4f502eb6d7a205a2c64f13ed627db878ca6
phs28 <104198099+phser29@users.noreply.github.com>
2024-10-21 08:43:40 +0900

    commit

--- backjun/src/book/chap05/exam01/BubbleSort.java
+++ backjun/src/book/chap05/exam01/BubbleSort.java
@@ -43,7 +43,11 @@ public class BubbleSort {
     while (k < n - 1) {
       int last = n - 1;
       for(int j=n-1; j>k; j--) {
-        
+        if(a[j - 1] > a[j]) {
+          swap(a, j-1, j);
+          last = j;
+        }
+        k = last;
       }
     }
   }
@@ -70,5 +74,4 @@ public class BubbleSort {
 
     scan. close();
   }
-
 }
