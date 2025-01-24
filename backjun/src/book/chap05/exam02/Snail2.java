174d924f33504ee5ce78be347bb388a705967043
phs28 <104198099+phser29@users.noreply.github.com>
2025-01-24 23:38:40 +0900

    commit

---
+++ backjun/src/book/chap05/exam02/Snail2.java
@@ -0,0 +1,26 @@
+package book.chap05.exam02;
+
+public class Snail2 {
+  public static void main(String[] args) {
+    int n = 5;
+    int arr[][] = new int[10][10];
+
+    for(int i=0; i<2*n-1; i++) {
+      switch (i % 4) {
+        case 0: // right way
+          
+          break;
+        case 1: // under way
+
+          break;
+        case 2: // left way
+
+          break;
+        case 3: // up way
+
+          break;
+      }
+    }
+
+  }
+}
